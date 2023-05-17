6e4d63da631e0064f81bc5ef23f7322fbda14f1f
Sadid <sadid.jones@gmail.com>
2023-05-17 22:11:11 +0600

    555

--- lib/prog_basic.dart
+++ lib/prog_basic.dart
@@ -1,1 +1,20 @@
+void main(List<String> args) {
+  final user = User('sadid');
+  final user2 = User('sadid 2');
+  var sum2 = user.add(a: 9, b: 8);
+  var sum3 = user.add(a: 9, b: 10);
+  // var sum = User().add(a: 5, b: 6);
+  print('sum + $sum2 + $sum3');
+}
 
+class User {
+  User(String name) {
+    print('Constructor Called Name $name');
+  }
+
+  int add({required int a, required int b}) {
+    return a + b;
+  }
+
+  void printName() {}
+}
