e3b871cfc8189e94eb7d182f154127d8878f6164
Sadid <sadid.jones@gmail.com>
2023-05-19 22:09:11 +0600

    @sss

--- lib/prog_basic.dart
+++ lib/prog_basic.dart
@@ -1,23 +1,52 @@
 void main(List<String> args) {
-  Cube b = Cube();
-  b.wsFunc();
-  b.cubeFunc();
+  HumanBeing humanBeing = HumanBeing();
+  humanBeing.hands();
 }
 
-class Ws {
-  Ws() {
-    print('Ws obj created!!!');
-  }
-  void wsFunc() {
-    print('Class Ws Function Called');
+class LivingBeing {
+  void hands() {
+    print('Used for eating');
   }
 }
 
-class Cube extends Ws {
-  Cube() {
-    print('Cube obj created!!!');
-  }
-  void cubeFunc() {
-    print('Class Cube Function Called');
+class Fish extends LivingBeing {
+  @override
+  void hands() {
+    print('Used for swimming');
   }
 }
+
+class HumanBeing extends LivingBeing {
+  @override
+  void hands() {
+    super.hands();
+    print('Used for Clapping!!');
+  }
+}
+
+class Ex extends HumanBeing {}
+
+
+// class Ws {
+//   Ws() {
+//     print('Ws obj created!!!');
+//   }
+//   void wsFunc() {
+//     print('Class Ws Function Called');
+//   }
+// }
+
+// class Cube extends Ws {
+//   Cube() {
+//     print('Cube obj created!!!');
+//   }
+//   void cubeFunc() {
+//     print('Class Cube Function Called');
+    
+//   }
+
+//   // @override
+//   // void wsFunc() {
+//   //   print('ddddddddd');
+//   // }
+// }
